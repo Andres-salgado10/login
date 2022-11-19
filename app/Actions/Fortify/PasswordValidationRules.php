7b2e772c1a610b31f34130cc0b436b7092e0f5c2
andres salgado <pipask_2012@hotmail.com>
2022-11-19 00:04:46 -0500

    contraseña

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -22,21 +22,22 @@ trait PasswordValidationRules
         ->symbols(1)
         ->uncompromised(), 
          'string', new Password, 'confirmed',
-         'g-recaptcha.response'=>function($atrribute,$value,$fail){
-            $secretkey=config('services.recapchat.secret');
-            $response=$value;
-            $userIP=$SERVER['REMOTE_ADDR']
-            $url="https://www.google.com/recaptcha/api/siteverify METHOD: POST";
-            $response =\file_get_contents($url);
-            $response=json_decode($response);
-            if(!$response->success){
-                session::flash('g-recaptcha-response','por favor marcar el recaptcha');
-                session::flash('alert-class','alert-danger');
-                $fail($attribute.'google recaptcha failed')
-            }
+         //'g-recaptcha.response'=>function($atrribute,$value,$fail){
+            //$secretkey=config('services.recapchat.secret');
+            //$response=$value;
+            //$userIP=$SERVER['REMOTE_ADDR']
+            //$url="https://www.google.com/recaptcha/api/siteverify";
+            //$response =\file_get_contents($url);
+            //$response=json_decode($response);
+            //if(!$response->success){
+                //session::flash('g-recaptcha-response','por favor marcar el recaptcha');
+                //session::flash('alert-class','alert-danger');
+                //$fail($attribute.'google recaptcha failed')
+            //}
 
 
-         }];
+        // }
+        ];
     }
 
 }
