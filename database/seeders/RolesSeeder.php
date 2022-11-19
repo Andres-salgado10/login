bb065fb1e245fb77071f772a614a5d85725bfcc9
andres salgado <pipask_2012@hotmail.com>
2022-11-18 20:29:29 -0500

    roles

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $role1 = Role::create(['name' => 'Admin']);
+       $role2 = Role::create(['name' => 'Usuario']);
+       
+    }
+}
